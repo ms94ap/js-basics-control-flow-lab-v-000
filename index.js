2d979a0e8002a28754fa9cd8fa1b9c895bebd7eb
Andrew Papacostas <a_papacostas@yahoo.com>
2018-02-24 14:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function scuberGreetingForFeet (feet) {
 function ternaryCheckCity(city) {
   return  city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
