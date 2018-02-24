bc0a508f3611775c8e74995971916f623c0b8362
Andrew Papacostas <a_papacostas@yahoo.com>
2018-02-24 14:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet(ride) {
-  if(ride <= 400) {
+function scuberGreetingForFeet(feet) {
+  if(feet <= 400) {
   return "This is on me!"
   }
 }
